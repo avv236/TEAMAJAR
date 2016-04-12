214c20f357958be5b2c0bd1762bf45fbe01ffb57
WAI WHA <WAI WHA>
2016-04-12 14:10:34 -0400

    xbox controller input detection (public vars)

---
+++ Assets/JoystickInputController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickInputController : MonoBehaviour {
+	public float joyX = 0f;
+	public float joyY = 0f;
+	public bool joyButton = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		joyButton = Input.GetButton ("JoystickJump");
+		joyX = Input.GetAxis ("JoystickX");
+		joyY = Input.GetAxis ("JoystickY");
+
+		if (joyButton) {
+			Debug.Log ("_A_ Pressed!");
+		}
+
+		if(joyX != 0f)
+			Debug.Log (Input.GetAxis ("JoystickX") + "");
+		if(joyY != 0f)
+			Debug.Log (Input.GetAxis ("JoystickY") + "");
+	}
+}
