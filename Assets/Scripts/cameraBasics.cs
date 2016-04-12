9b1874cacc24060cac52b543722273b3fbdb1295
unknown <ayl292@nyu.edu>
2016-04-12 14:02:11 -0400

    working basic camera

---
+++ Assets/Scripts/cameraBasics.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraBasics : MonoBehaviour {
+
+	public Transform p1;
+	public Transform p2;
+	public Transform p3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 midpoint = new Vector3((p1.transform.position.x + p2.transform.position.x + p3.transform.position.x) / 3,
+									(p1.transform.position.y + p2.transform.position.y + p3.transform.position.y) / 3,
+									-10);
+		
+		transform.position = Vector3.Lerp(transform.position, midpoint, 0.2f);
+	}
+}
--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class testScript : MonoBehaviour {
 
+	public GameObject cube1;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,11 @@ public class testScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 fMovement = transform.up * 5f * vertical;
+		transform.position += fMovement * Time.deltaTime;
 	}
+
 }
