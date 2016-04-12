1b75ace0ade5400f955a2c4d5b6ee422f66a3c0a
Audrey V <avv236@nyu.edu>
2016-04-12 16:49:38 -0400

    COOL JUMPING

--- Assets/Scripts/Jumping.cs
+++ Assets/Scripts/Jumping.cs
@@ -4,10 +4,11 @@ using System.Collections;
 
 public class Jumping : MonoBehaviour {
 
-    float timer = 0f;
-    public float jumpSpeed = 5f;
-    public float fallSpeed = 5f;
-    public float jumpHeight = 2f;
+    float jumpTimer = 0f;
+    public float jumpSpeed = 20f;
+    public float fallSpeed = 1f;
+    public float jumpHeight = 20f;
+    public float jumpMultiplier = 2f;
     bool isJumping = false;
     Rigidbody rb;
 
@@ -16,6 +17,27 @@ public class Jumping : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
     }
 
+    void Update(){
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jumpTimer += Time.deltaTime;
+
+            if (jumpTimer > 1.5f)
+            {
+                jumpTimer = 1.5f;
+            }
+          
+            Debug.Log(jumpTimer);
+        }
+        else
+        {
+            jumpTimer = 0f;
+        }
+
+
+    }
+
+
     void FixedUpdate () {
         if(GetComponent<Rigidbody>().velocity.y == 0f)
         {
@@ -23,17 +45,7 @@ public class Jumping : MonoBehaviour {
             transform.localScale = new Vector3(1f, 1f, 1f);
             transform.position = new Vector3(transform.position.x, 0.75f, transform.position.z);
         }
-
-       /* if (Input.GetKeyDown(KeyCode.Space))
-        {
-            timer += Time.deltaTime;
-            if(timer > 3f)
-            {
-                timer = 3f;
-            }
-        }
-        */
-
+        
         if (Input.GetKey(KeyCode.Space) && isJumping == false)
         {
             transform.localScale = new Vector3(1f, 0.5f, 1f);
@@ -44,7 +56,7 @@ public class Jumping : MonoBehaviour {
         {
             transform.localScale = new Vector3(1f, 1f, 1f);
             //rb.AddRelativeForce(0f, 300f, 0f);
-            rb.velocity += Vector3.up * jumpSpeed;
+            rb.velocity += Vector3.up * jumpSpeed * (jumpTimer * jumpMultiplier);
             isJumping = true;
         }
         
