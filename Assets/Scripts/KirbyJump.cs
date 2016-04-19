49a77d3b8c827662799613c49511c9d14e65ccbe
Audrey V <avv236@nyu.edu>
2016-04-19 16:45:44 -0400

    robert fixed the jumping lag!!!!!!!!!!!!!!!

--- Assets/Scripts/KirbyJump.cs
+++ Assets/Scripts/KirbyJump.cs
@@ -9,7 +9,7 @@ public class KirbyJump : MonoBehaviour {
     public float jumpSpeed = 8f;
     public float fallingGravity = 1.5f;
     public float gravity = 20f;
-    float jumpCounter = 10f;
+    int jumpCounter = 10;
     float jumpsLeft;
     public GameObject staminaBar;
     Vector3 moveDirection = Vector3.zero;
@@ -23,49 +23,36 @@ public class KirbyJump : MonoBehaviour {
         if(Physics.Raycast(rayGrounded, 1f))
         {
             grounded = true;
-            jumpCounter = 10f;
+            jumpCounter = 10;
             moveDirection.y = 0;
         }
         else
         {
             grounded = false;
         }
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
 
         CharacterController controller = GetComponent<CharacterController>();
+        moveDirection.x = Input.GetAxis("Horizontal") * speed;
+        moveDirection.z = Input.GetAxis("Vertical") * speed;
 
-        Debug.Log(controller.isGrounded);
-        if (jumpCounter > 0f)
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (jumpCounter >= 0f)
         {
-            moveDirection.x = Input.GetAxis("Horizontal") * speed;
-            moveDirection.z = Input.GetAxis("Vertical") * speed;
-
-            moveDirection = transform.TransformDirection(moveDirection);
-
             if (Input.GetButtonDown("Jump"))
             {
                 Debug.Log("pressed");
                 moveDirection.y = jumpSpeed * jumpCounter;
                 jumpCounter--;
-            }
-
-            if(moveDirection.y < 0f)
-            {
-                gravity += fallingGravity;
-
-                if (moveDirection.y <= 0f)
-                {
-                    gravity = 9.8f;
-                }
+                
             }
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
 
-        jumpsLeft = jumpCounter / 10;
-        staminaBar.transform.localScale = new Vector3(jumpsLeft, 1, 1);
+        //Stamina bar
+        jumpsLeft = 1f * jumpCounter / 10f;
+        //staminaBar.transform.localScale = new Vector3(jumpsLeft, 1, 1);
+        staminaBar.GetComponent<Image>().fillAmount = jumpsLeft;
     }
 }
