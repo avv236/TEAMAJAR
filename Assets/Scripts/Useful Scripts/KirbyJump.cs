fc9da9a649fe4b3404a42be8e2c1cac7808e2397
Audrey V <avv236@nyu.edu>
2016-04-26 16:32:43 -0400

    Heavy jump

--- Assets/Scripts/Useful Scripts/KirbyJump.cs
+++ Assets/Scripts/Useful Scripts/KirbyJump.cs
@@ -8,6 +8,7 @@ public class KirbyJump : MonoBehaviour {
     public float speed = 6f;
     public float jumpSpeed = 8f;
     public float fallingGravity = 1.5f;
+    float initialGravity = 11f;
     public float gravity = 20f;
     int jumpCounter = 10;
     float jumpsLeft;
@@ -37,6 +38,7 @@ public class KirbyJump : MonoBehaviour {
 
         moveDirection = transform.TransformDirection(moveDirection);
 
+
         if (jumpCounter >= 0f)
         {
             if (Input.GetButtonDown("Jump"))
@@ -45,6 +47,15 @@ public class KirbyJump : MonoBehaviour {
                 moveDirection.y = jumpSpeed * jumpCounter;
                 jumpCounter--;
                 
+            }
+
+        if( moveDirection.y < 0f)
+            {
+                gravity = fallingGravity;
+            }
+            else
+            {
+                gravity = initialGravity;
             }
         }
         moveDirection.y -= gravity * Time.deltaTime;
