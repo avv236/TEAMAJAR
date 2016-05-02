e2e2deed4d76b6bdc0fef3d83004fec1b5e2cdaf
Joel Forrester <joelforr@gmail.com>
2016-05-02 16:51:32 -0400

    drop totem stuff  **not using anymore?**

--- Assets/Scripts/Useful Scripts/PlayerBlockSpawn.cs
+++ Assets/Scripts/Useful Scripts/PlayerBlockSpawn.cs
@@ -4,23 +4,43 @@ using System.Collections.Generic;
 
 public class PlayerBlockSpawn : MonoBehaviour {
 
+	//PLAYER VARIABLES
+	public Vector3 playerPosition;
+	public Renderer playerRend;
+
+	//BLOCK VARIABLES
 	public Transform BlockPrefab;
 	List<Transform> listOfBlocks = new List<Transform>();
 
 	public Vector3 blockScale;
-	public Vector3 playerPosition;
 	public Vector3 spawnPosition;
+
+	//block spawn cooldown vars
+	public float nextBlock = 0f;
+	public float blockCooldown = 1f;
+
+	//TOTEM/SIGIL VARIABLES
+	public Transform totemA;
+	public Transform totemB;
+	public Transform totemC;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		playerRend = GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		playerPosition = transform.position;
 
-		DropBlock ();
+		if(Time.time < nextBlock){
+			Debug.Log (Time.time);
+		}
+		if(Time.time > nextBlock && playerRend.enabled){
+			DropBlock ();
+		}
+
 	}
 
 	void DropBlock(){
@@ -43,7 +63,27 @@ public class PlayerBlockSpawn : MonoBehaviour {
 				// if there isnt spawn block;
 				Transform newBlock = (Transform)Instantiate (BlockPrefab,spawnPosition,Quaternion.identity);
 				listOfBlocks.Add(newBlock);
+				nextBlock = Time.time + blockCooldown;
+				Debug.Log ("Next Block Available at:" + nextBlock);
 			}
 		}
 	}
+
+	void DropTotem(){
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			spawnPosition = new Vector3 (Mathf.RoundToInt(playerPosition.x), Mathf.RoundToInt(playerPosition.y), Mathf.RoundToInt(playerPosition.z)) + (-transform.up*2f);
+			Debug.Log (spawnPosition);
+
+			Ray spawnCheck = new Ray (playerPosition, Vector3.down);
+			RaycastHit spawnCheckInfo = new RaycastHit ();
+
+			if(Physics.SphereCast(playerPosition,.49f, Vector3.down, out spawnCheckInfo,1f)){
+				Debug.Log ("Not here");
+			}else{
+				Transform newTotemA = (Transform)Instantiate (totemA, spawnPosition, Quaternion.identity);
+
+			}
+		}
+	}
+
 }
