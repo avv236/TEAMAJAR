d4e09ee0bc771d5ea6b7f77c51a4415eef353f83
Audrey V <avv236@nyu.edu>
2016-05-11 15:39:38 -0400

    Title screen half done

---
+++ Assets/Scripts/Useful Scripts/clubTitleMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clubTitleMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x >= 200f)
+        {
+            transform.position -= new Vector3(10f, 0f, 0f) * Time.deltaTime * 40f;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Useful Scripts/flightTitleMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flightTitleMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       /* transform.position += new Vector3(10f, 0f, 0f) * Time.deltaTime * 40f;
+
+        if(transform.position.x >= 0f)
+        {
+            Debug.Log("Passed 0");
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+           
+        }
+        */
+
+        if(transform.position.x <= 200f)
+        {
+            transform.position += new Vector3(10f, 0f, 0f) * Time.deltaTime * 40f;
+        }
+
+	}
+}
